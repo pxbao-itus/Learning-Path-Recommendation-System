daf3831fb2811ed97d6467f9c21e62c28f13ad9b
Bao Phan Xuan <pxbao@tma.com.vn>
2022-05-10 16:33:30 +0700

    init neo4j connection app and config dotenv file

---
+++ Neo4j_Connection/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from neomodel import Q, db
+# Create your models here.
+
+db.set_connection()
--- RecommendationSystem/settings.py
+++ RecommendationSystem/settings.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from neomodel import config
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Connection for neo4j
+config.DATABASE_URL = 'bolt://' + os.getenv('USERNAME') + ":" + os.getenv('PASSWORD') + "@" + os.getenv('URL')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,7 +31,6 @@ SECRET_KEY = 'django-insecure-t7qbc(yka0^b&kr1pbe#s&rl_7lbg*-iq)vg@%3aw-x5euufkf
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'RecommendationSystem.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -79,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -99,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -110,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
