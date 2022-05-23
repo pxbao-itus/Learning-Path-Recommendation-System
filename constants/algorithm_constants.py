b8dd123f74ee0299a4201f41088dc3a7c51b67b6
Bao Phan Xuan <pxbao@tma.com.vn>
2022-05-23 17:24:03 +0700

    init algorithm for step 1

---
+++ constants/algorithm_constants.py
@@ -0,0 +1,6 @@
+
+class AlgorithmConstant:
+    alpha = 1
+    beta = 1
+    lambda_ = 1
+    denta = 1
--- models/connection.py
+++ models/connection.py
@@ -1,7 +1,12 @@
 import os
-
 from dotenv import load_dotenv
+from py2neo import Graph
 from neomodel import Q, db
 
+
+# connecting for neomodel
 load_dotenv()
 db.set_connection('bolt://' + os.getenv('USERNAME') + ":" + os.getenv('PASSWORD') + "@" + os.getenv('URL'))
+
+# connecting for py2neo
+g = Graph("bolt://localhost:7687", auth=("neo4j", "fit@hcmus"))
