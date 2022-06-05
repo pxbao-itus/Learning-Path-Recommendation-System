c6fd8b60ad274d26b73ee3fd20c62ec005a16241
pxbao <pxbao.itus@gmail.com>
2022-06-05 09:07:08 +0700

    add instruction for api

--- apis/urls.py
+++ apis/urls.py
@@ -16,9 +16,84 @@ urlpatterns = [
     path('lo/platform', learning_object_apis.get_all_platform),
     path('lo/framework', learning_object_apis.get_all_framework),
 
+    # url: http://localhost:8000/apis/user/create
+    # method: POST
+    # body_example: {
+    #     "username": "Bob123",
+    #     "password": "123123",
+    #     "name": "Bob"
+    # }
+    # response: {
+    #      "status": "success" | "fail"
+    # }
     path('user/create', user_apis.create_user),
+
+    # url: http://localhost:8000/apis/user/info?id=4684
+    # method: GET
+    # response: {
+    #     "id": 4684,
+    #     "username": "Bob123",
+    #     "name": "Bob"
+    # }
     path('user/info/', user_apis.get_user_info),
+
+    # url: http://localhost:8000/apis/user/objective
+    # method: POST
+    # body_example: {
+    #     "user_id": 4686,
+    #     "career_id": 35
+    # }
+    # response: {
+    #      "status": "success" | "fail"
+    # }
     path('user/objective', user_apis.create_objective_career),
+
+    # url: http://localhost:8000/apis/user/has
+    # method: POST
+    # body_example: {
+    #     "user_id": 4684,
+    #     "list_lo": [
+    #         {
+    #             "id": 22,
+    #             "type": "tool",
+    #             "level": 1
+    #         },
+    #         {
+    #             "id": 16,
+    #             "type": "knowledge",
+    #             "level": 1
+    #         },
+    #         {
+    #             "id": 27,
+    #             "type": "platform",
+    #             "level": 1
+    #         },
+    #         {
+    #             "id": 91,
+    #             "type": "framework",
+    #             "level": 1
+    #         }
+    #     ]
+    # }
+    # response: {
+    #      "status": "success" | "fail"
+    # }
     path('user/has', user_apis.create_user_has_lo),
+
+    # url: http://localhost:8000/apis/user/learning-path?id=4685
+    # method: GET
+    # response: [
+    # [
+    #     2081,
+    #     867,
+    #     3269,
+    #     1905,
+    #     4404,
+    #     2153,
+    #     4415,
+    #     4417,
+    #     3161
+    # ]
+    # ]
     path('user/learning-path', user_apis.get_learning_path)
 ]
