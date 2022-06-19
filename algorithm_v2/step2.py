8a2a8a3c0496443f068b18f697d476e567c75bb2
kyle <leanhtuanlhln@gmail.com>
2022-06-19 14:26:57 +0700

    complete algorithm v2

--- algorithm_v2/step2.py
+++ algorithm_v2/step2.py
@@ -21,7 +21,7 @@ def run_genetic_algorithm(all_set_of_candidate_course,user):
     native_population = initial_population(all_set_of_candidate_course,LO_imax)
     loop_times = 0
     selected_population_dict = []
-    while(loop_times < AlgorithmConstant.GENETIC_LOOP_TIMES):
+    while(loop_times < AlgorithmConstant.V2_GENETIC_LOOP_TIMES):
         selected_population_dict = evaluate_and_filter(native_population,length_of_population,user)
         n = selected_population_dict.__len__()
         while(n < length_of_population):
@@ -42,16 +42,16 @@ def finding_optimization_courses(all_set_of_candidate_course,user):
     filter_set_of_course = []
 
     # Main Process
-    set_of_course_with_score = run_genetic_algorithm(all_set_of_candidate_course,user)
-    if not set_of_course_with_score:
+    population_with_score = run_genetic_algorithm(all_set_of_candidate_course,user)
+    if not population_with_score:
         print("Can not find set of course from running genetic algorithm")
         return
     if(user_need_time == 0 and user_need_cost == 0):
-        filter_set_of_course = set_of_course_with_score
+        filter_set_of_course = population_with_score
     elif(user_need_time != 0):
-        filter_set_of_course = user_filter_time(user_need_time,set_of_course_with_score)
+        filter_set_of_course = user_filter_time(user_need_time,population_with_score)
     elif(user_need_cost != 0):
-        filter_set_of_course = user_filter_cost(user_need_cost,set_of_course_with_score)
+        filter_set_of_course = user_filter_cost(user_need_cost,population_with_score)
 
     # Return
     return get_set_of_course_by_omega(filter_set_of_course)
@@ -105,7 +105,7 @@ def evaluate_and_filter(native_population,length_of_population,user):
         individual_score = calculate_score_for_individual(individual,user)
         set_of_score_list.update({individual:individual_score})
     sort_score_list = dict_sorted_by_value(set_of_score_list)
-    n = math.floor(AlgorithmConstant.LAMBDA * length_of_population)
+    n = math.floor(AlgorithmConstant.V2_LAMBDA * length_of_population)
     for individual in sort_score_list:
         if len(population_filtered) == n:
             break
@@ -129,7 +129,7 @@ def rebuild_population(selected_population_dict,all_set_of_candidate_course):
     individual_1 = get_individual_by_index(selected_population_dict,index_1)
     individual_2 = get_individual_by_index(selected_population_dict,index_2)
     ratio = random.random()
-    if(ratio > AlgorithmConstant.ALPHA):
+    if(ratio > AlgorithmConstant.V2_ALPHA):
         new_individuals = crossover(individual_1,individual_2)
         selected_population_dict.update({new_individuals[0]:calculate_score_for_individual(new_individuals[0],user)})
         selected_population_dict.update({new_individuals[1]:calculate_score_for_individual(new_individuals[1],user)})
@@ -158,45 +158,36 @@ def crossover(individual_1,individual_2):
     return [individual_1,individual_2]
 
 def calculate_score_for_individual(individual,user):
-    thisset = {}
-    for gen in individual:
-        if type(gen) == type([]):
-            for course_id in gen:
-                thisset.add(course_id)
-        elif (type(gen) == type(1)):
-            thisset.add(gen)
-        else:
-            print("type of gen in individual is not true")
-            return
+    
     user_lo_need =  graph.run(query_get_user_need_lo(user.id)).data()
-
-    f1 = calculate_number_of_course(thisset)
-    f2 = calculate_number_of_redundant_LO(thisset,user_lo_need)
-    f3 = calculate_number_of_overlap_LO(thisset)
-    f4 = calculate_number_of_overlap_level(thisset,user_lo_need)
-    w1 = AlgorithmConstant.W1
-    w2 = AlgorithmConstant.W2
-    w3 = AlgorithmConstant.W3
-    w4 = AlgorithmConstant.W4
+    set_of_course = get_set_of_course_from_individual(individual)
+    f1 = calculate_number_of_course(set_of_course)
+    f2 = calculate_number_of_redundant_LO(set_of_course,user_lo_need)
+    f3 = calculate_number_of_overlap_LO(set_of_course)
+    f4 = calculate_number_of_overlap_level(set_of_course,user_lo_need)
+    w1 = AlgorithmConstant.V2_W1
+    w2 = AlgorithmConstant.V2_W2
+    w3 = AlgorithmConstant.V2_W3
+    w4 = AlgorithmConstant.V2_W4
     weighted_sum = f1*w1 + f2*w2 + f3*w3 + f4*w4
     return weighted_sum
 
-def calculate_number_of_course(thisset):
-    return len(thisset)
+def calculate_number_of_course(set_of_course):
+    return len(set_of_course)
 
-def calculate_number_of_redundant_LO(thisset,user_lo_need):
-    lo_provided = graph.run(query_to_get_list_lo_provided_by_set_course(list(thisset))).data()
+def calculate_number_of_redundant_LO(set_of_course,user_lo_need):
+    lo_provided = graph.run(query_to_get_list_lo_provided_by_set_course(set_of_course)).data()
     set_lo = list(set(get_list_id(lo_provided)))
     return set_lo.__len__() - user_lo_need.__len__()
 
-def calculate_number_of_overlap_LO(thisset):
-    lo_provided = graph.run(query_to_get_list_lo_provided_by_set_course(list(thisset))).data()
+def calculate_number_of_overlap_LO(set_of_course):
+    lo_provided = graph.run(query_to_get_list_lo_provided_by_set_course(set_of_course)).data()
     set_lo = list(set(get_list_id(lo_provided)))
     return lo_provided.__len__() - set_lo.__len__()
 
-def calculate_number_of_overlap_level(thisset,user_lo_need):
+def calculate_number_of_overlap_level(set_of_course,user_lo_need):
     counter = 0
-    lo_provided = graph.run(query_to_get_list_lo_provided_by_set_course(list(thisset))).data()
+    lo_provided = graph.run(query_to_get_list_lo_provided_by_set_course(set_of_course)).data()
 
     for lo in user_lo_need:
         for lo_provided_element in lo_provided:
@@ -204,6 +195,22 @@ def calculate_number_of_overlap_level(thisset,user_lo_need):
                 counter += lo_provided_element.get('level') - lo.get('level')
                 break
     return counter
+
+def calculate_sum_time_of_individual(set_of_course):
+    list_time = graph.run(query_get_list_time_of_set_course(set_of_course)).data()
+    total_time = 0
+
+    for time in list_time:
+        try:
+            total_time += float(time.get('time')[0: time.get('time').find(" ")])
+        except:
+            total_time += 0
+
+    return total_time
+
+def calculate_sum_cost_of_individual(set_of_course):
+    sumFee = graph.run(query_get_sum_tuition_set_course(set_of_course)).data()
+    return sumFee[0].get('sumFee') or 0
 
 def get_individual_by_index(selected_population_dict,index):
     pivot = 0
@@ -219,15 +226,25 @@ def get_individual_by_index(selected_population_dict,index):
         return
     return individual
 
-def sort_individual_by_score(set_of_score_list):
+def user_filter_time(user_need_time,population_with_score):
+    filter_set_of_course = {}
+    set_of_individual = population_with_score.keys()
+    for individual in set_of_individual:
+        set_of_course = get_set_of_course_from_individual(individual)
+        sum_time_set_of_course = calculate_sum_time_of_individual(set_of_course)
+        if(user_need_time >= sum_time_set_of_course):
+            filter_set_of_course.update({set_of_course: population_with_score[individual]})
+    return filter_set_of_course
 
-    return
-
-def user_filter_time(user_need_time,optimized_set_of_course):
-    return
-
-def user_filter_cost(user_need_time,optimized_set_of_course):
-    return
+def user_filter_cost(user_need_cost,population_with_score):
+    filter_set_of_course_with_score = {}
+    set_of_individual = population_with_score.keys()
+    for individual in set_of_individual:
+        set_of_course = get_set_of_course_from_individual(individual)
+        sum_cost_set_of_course = calculate_sum_cost_of_individual(set_of_course)
+        if(user_need_cost >= sum_cost_set_of_course):
+            filter_set_of_course_with_score.update({set_of_course: population_with_score[individual]})
+    return filter_set_of_course_with_score
 
 def get_set_of_course_by_omega(filter_set_of_course):
     sorted_set_of_course_by_score = dict_sorted_by_value(filter_set_of_course)
@@ -255,3 +272,18 @@ def get_list_id(list_dict):
 
 def dict_sorted_by_value(dictionary):
     sorted_dict = dict(sorted(dictionary.items(), key=lambda item: item[1]))
+    return sorted_dict
+
+def get_set_of_course_from_individual(individual):
+    list_set = {}
+    for gen in individual:
+        if type(gen) == type([]):
+            for course_id in gen:
+                list_set.add(course_id)
+        elif (type(gen) == type(1)):
+            list_set.add(gen)
+        else:
+            print("type of gen in individual is not true")
+            return
+    return list(list_set)
+
